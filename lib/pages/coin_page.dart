347f3edc190b8eca126d4a9aba28410c0bc7ea5d
Leandro Ojeda <leandro.ojeda@marandu.com.ar>
2023-10-25 21:44:10 -0300

    Chore (Url-coin): less result on url

--- lib/pages/coin_page.dart
+++ lib/pages/coin_page.dart
@@ -21,7 +21,7 @@ class _CoinPageState extends State<CoinPage> {
   Future<List<Coin>> fetchCoin() async {
     coinList = [];
     final response = await http.get(Uri.parse(
-        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=30&page=1&sparkline=false&locale=en'));
+        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false&locale=en'));
     if (response.statusCode == 200) {
       List<dynamic> values = [];
       values = json.decode(response.body);
